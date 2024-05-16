75e4eddd9f611e37c7eacdcccd173c49cebc720f
illyalane <lane_illya@yahoo.com>
2024-05-16 15:32:11 -0400

    completed lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,55 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        #if items is None:
+        #    items = []
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        
+        return len(self.items) == 0
 
+    def empty(self):
+        
+        return self.isEmpty()
+
+    
     def push(self, item):
-        pass
+        if  not self.full():
+            self.items.append(item)
+        
+            return self
+
 
     def pop(self):
-        pass
+        
+        if len(self.items) > 0:
+            return self.items.pop()
+        else:
+            return None   
 
     def peek(self):
-        pass
+        
+        if len(self.items) > 0:
+            return self.items[-1]
+        else:
+            return None
+        
     
     def size(self):
-        pass
-
+        
+        return len(self.items)
+        
     def full(self):
-        pass
+        
+        return self.size() == self.limit
 
     def search(self, target):
-        pass
+        # Iterate through the stack starting from the top
+        for i in range(len(self.items) - 1, -1, -1):
+            if self.items[i] == target:
+            # Return the distance from the top to the matched element
+                return len(self.items) - i - 1
+    # If target element is not found, return -1
+        return -1
